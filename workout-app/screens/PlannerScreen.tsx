1db3e38987c63adf4d65715dedcb28fe9ecc2a2b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-01 14:03:03 +0300

    Added some basic logic to the PlannerScreen component

---
+++ workout-app/screens/PlannerScreen.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+
+export default function PlannerScreen()
+{
+    return (
+        <View>
+            <Text>I am PlannerScreen</Text>
+        </View>
+    );
+};
