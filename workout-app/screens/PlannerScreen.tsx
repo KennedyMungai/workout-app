913233642bf266120f1b234c561fca6c4bd100b7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-01 15:12:35 +0300

    Added an import for the useEffect hook and implemented it to run once the PLanner component rendered

--- workout-app/screens/PlannerScreen.tsx
+++ workout-app/screens/PlannerScreen.tsx
@@ -1,8 +1,15 @@
+import { useEffect } from "react";
 import { Text, View, Button } from "react-native";
 
 
 export default function PlannerScreen({ navigation })
 {
+    useEffect(() =>
+    {
+        console.log("Rendering the planner screen")
+    }, []);
+
+
     return (
         <View>
             <Text>I am PlannerScreen</Text>
