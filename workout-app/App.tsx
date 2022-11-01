22fb616898b242047d14550dd4c3a8756473d6dd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-01 14:38:19 +0300

    Added an import for the Navigation component and rendered it out

--- workout-app/App.tsx
+++ workout-app/App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
+import Navigation from './navigation';
 
 
 export default function App()
 {
   return (
     <View>
+      <Navigation />
       <StatusBar style="auto" />
     </View>
   );
