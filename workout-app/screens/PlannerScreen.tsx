885203b22a34a369a942c351eb53a58e778d4aab
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-01 15:01:26 +0300

    Added an import for the Button component

--- workout-app/screens/PlannerScreen.tsx
+++ workout-app/screens/PlannerScreen.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 
 
 export default function PlannerScreen()
@@ -6,6 +6,7 @@ export default function PlannerScreen()
     return (
         <View>
             <Text>I am PlannerScreen</Text>
+            <Button title="Go to Planner" onPress={() => alert("Go to planner")} />
         </View>
     );
 };
