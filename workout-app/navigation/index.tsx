70b4018686e89c3368c00a4537dd321f3d4e9f63
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-01 14:41:36 +0300

    Formatted the document

--- workout-app/navigation/index.tsx
+++ workout-app/navigation/index.tsx
@@ -3,22 +3,16 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
 import PlannerScreen from "../screens/PlannerScreen";
 
-
 const Stack = createNativeStackNavigator();
-
 
 export default function Navigation()
 {
-    <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-            <Stack.Screen
-                name="Home"
-                component={HomeScreen}
-            />
-            <Stack.Screen
-                name="Planner"
-                component={PlannerScreen}
-            />
-        </Stack.Navigator>
-    </NavigationContainer>
-};
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName="Home">
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Planner" component={PlannerScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
