fe014af5a25eb09a3097f59506405ea0f82d615a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-01 15:08:23 +0300

    Added navigation code for the Go to Home button

--- workout-app/screens/PlannerScreen.tsx
+++ workout-app/screens/PlannerScreen.tsx
@@ -1,12 +1,12 @@
 import { Text, View, Button } from "react-native";
 
 
-export default function PlannerScreen()
+export default function PlannerScreen({ navigation })
 {
     return (
         <View>
             <Text>I am PlannerScreen</Text>
-            <Button title="Go to Home" onPress={() => alert("Go to Home")} />
+            <Button title="Go to Home" onPress={() => navigation.push("Home")} />
         </View>
     );
 };
