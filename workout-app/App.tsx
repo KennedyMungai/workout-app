10b7dbf43b03836888a1c4de470feb17d6342034
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-01 14:36:28 +0300

    Removed the PLannerScreen and HomeSCreen components and their imports

--- workout-app/App.tsx
+++ workout-app/App.tsx
@@ -1,15 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
-import PlannerScreen from './screens/PlannerScreen';
-// import HomeScreen from './screens/HomeScreen';
 
 
 export default function App()
 {
   return (
     <View>
-      {/* <HomeScreen /> */}
-      <PlannerScreen />
       <StatusBar style="auto" />
     </View>
   );
