6c67da73fe5115e030468611ada42003a74e783d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-01 14:26:14 +0300

    Initialized the createNativeStackNavigator function

--- workout-app/navigation/index.tsx
+++ workout-app/navigation/index.tsx
@@ -2,3 +2,14 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
 import PlannerScreen from "../screens/PlannerScreen";
+
+
+const Stack = createNativeStackNavigator();
+
+
+export default function Navigation()
+{
+    <NavigationContainer>
+
+    </NavigationContainer>
+};
