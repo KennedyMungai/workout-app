f716de58b2ae2aa09562fe9e8dddbc768000171d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-01 14:09:29 +0300

    Commented out the HomeScreen component and its import

--- workout-app/App.tsx
+++ workout-app/App.tsx
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
-import HomeScreen from './screens/HomeScreen';
+// import HomeScreen from './screens/HomeScreen';
 
 
 export default function App()
 {
   return (
     <View>
-      <HomeScreen />
+      {/* <HomeScreen /> */}
       <StatusBar style="auto" />
     </View>
   );
