f974f134f066683d534d881844aa9273c9a5868c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-01 15:06:23 +0300

    Added logic to show the Planner cmponent when the Go to PLanner button is pressed

--- workout-app/screens/HomeScreen.tsx
+++ workout-app/screens/HomeScreen.tsx
@@ -1,12 +1,12 @@
 import { Text, View, Button } from "react-native";
 
 
-export default function HomeScreen()
+export default function HomeScreen({ navigation })
 {
     return (
         <View>
             <Text>I am HomeScreen</Text>
-            <Button title="Go to Planner" onPress={() => alert("Go to planner")} />
+            <Button title="Go to Planner" onPress={() => navigation.push("Planner")} />
         </View>
     );
 };
