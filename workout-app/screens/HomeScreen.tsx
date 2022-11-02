a778ace52d75bb06416dd7dc3aebdb5ee2455452
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-02 15:30:11 +0300

    Implemented the FlatList component

--- workout-app/screens/HomeScreen.tsx
+++ workout-app/screens/HomeScreen.tsx
@@ -9,6 +9,7 @@ export default function HomeScreen({ navigation }: NativeStackHeaderProps)
         <View style={styles.container}>
             <Text>I am HomeScreen</Text>
             <Text>{JSON.stringify(data)}</Text>
+            <FlatList />
         </View>
     );
 };
