63a704f1aa29469707b9b82fdd0811ca4733c069
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-02 11:01:01 +0300

    CHanged the name from Home to Root

--- workout-app/navigation/index.tsx
+++ workout-app/navigation/index.tsx
@@ -21,7 +21,7 @@ function RootNavigator()
 {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Root" component={HomeScreen} />
             <Stack.Screen name="Planner" component={PlannerScreen} />
         </Stack.Navigator>
     );
