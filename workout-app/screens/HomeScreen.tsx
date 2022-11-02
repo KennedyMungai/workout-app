1257c4a0327774b273fbf468e07327b62934f544
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-02 12:48:55 +0300

    Removed the useEffect statement and the import of the hook

--- workout-app/screens/HomeScreen.tsx
+++ workout-app/screens/HomeScreen.tsx
@@ -1,18 +1,9 @@
 import { Text, View, Button } from "react-native";
 import { NativeStackHeaderProps } from '@react-navigation/native-stack';
-import { useEffect } from "react";
 
 
 export default function HomeScreen({ navigation }: NativeStackHeaderProps)
 {
-    useEffect(() =>
-    {
-        console.log("Rendering the Home screen");
-
-        return () => console.log("Unmounting Home Screen");
-    }, []);
-
-
     return (
         <View>
             <Text>I am HomeScreen</Text>
