f3eaefcb92be35bfb73252e7bd40513d4953e3d4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-02 11:22:04 +0300

    Added the useEffect hook to the xomponent

--- workout-app/screens/HomeScreen.tsx
+++ workout-app/screens/HomeScreen.tsx
@@ -1,9 +1,16 @@
 import { Text, View, Button } from "react-native";
 import { NativeStackHeaderProps } from '@react-navigation/native-stack';
+import { useEffect } from "react";
 
 
 export default function HomeScreen({ navigation }: NativeStackHeaderProps)
 {
+    useEffect(() =>
+    {
+        console.log("Rendering the planner screen")
+    }, []);
+
+
     return (
         <View>
             <Text>I am HomeScreen</Text>
