4de80ba9914f6dbdb77e30ed4274eecc22060f12
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-02 11:40:22 +0300

    Made the test screen button use push instead of navigate

--- workout-app/screens/TestScreen.tsx
+++ workout-app/screens/TestScreen.tsx
@@ -16,7 +16,7 @@ export default function TestScreen({ navigation }: NativeStackHeaderProps)
     return (
         <View>
             <Text>I am HomeScreen</Text>
-            <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+            <Button title="Go to Home" onPress={() => navigation.push("Home")} />
         </View>
     );
 };
