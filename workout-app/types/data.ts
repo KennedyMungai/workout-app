bab829a4a0dfd0ef22e2ca5a19d522b1c3c4e32f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-03 15:32:23 +0300

    Defined an interface pof type Sequence Item

--- workout-app/types/data.ts
+++ workout-app/types/data.ts
@@ -1,5 +1,12 @@
 export type Difficulty = "easy" | "normal" | "hard";
 
+export interface SequenceItem {
+    slug: string,
+    name: string,
+    type: "exercise" | "break" | "stretch",
+    duration: number,
+}
+
 export interface WorkOut {
     slug: string,
     name: string,
