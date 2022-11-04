5e81f09e41f45cd3d374bb0c506306905f3b3a66
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-04 10:43:23 +0300

    Created the isLoadingComplete state

--- workout-app/hooks/useCachedResources.ts
+++ workout-app/hooks/useCachedResources.ts
@@ -3,9 +3,7 @@ import React, { useState, useEffect } from 'react';
 
 const useCachedResources = () =>
 {
-    return (
-        
-    )
+    const [isLoadingComplete, setisLoadingComplete] = useState<boolean>(false);
 }
 
 export default useCachedResources;
