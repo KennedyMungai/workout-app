3e3141cab6e653c7972d5c0247d4787812432fe6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-04 12:44:30 +0300

    Added logic to the formatSec function

--- workout-app/utils/time.ts
+++ workout-app/utils/time.ts
@@ -6,7 +6,10 @@ export const secToMin = (sec: number): string =>
     return (sec / 60).toFixed(1);
 }
 
-export const formatSec = (sec: number) => 
+export const formatSec = (sec: number): string => 
 {
+    const _min = Math.floor(sec / 60);
+    const _sec = sec % 60;
 
+    return `${_min} minutes { _sec } seconds`;
 };
