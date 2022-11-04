a2190da437e6816b57f56beaa7400a3da5add1eb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-04 08:47:48 +0300

    Used the proper labelling for the different properties

--- workout-app/components/WorkoutItem.tsx
+++ workout-app/components/WorkoutItem.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-import { WorkOut } from '../types/data';
+import { WorkOut, Difficulty } from '../types/data';
 
 
 const WorkoutItem = ({ item }: { item: WorkOut }) =>
 {
     return (
         <View>
-            <Text>{item.name}</Text>
-            <Text>{item.duration}</Text>
-            <Text>{item.difficulty}</Text>
+            <Text>Name: {item.name}</Text>
+            <Text>Duration: {item.duration}</Text>
+            <Text>Difficulty: {item.difficulty}</Text>
         </View>
     )
 }
