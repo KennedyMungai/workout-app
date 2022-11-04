fc615fdd51868a7a082610d3404a2f3ed0b4e2b1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-04 11:27:48 +0300

    Loaded the fonts

--- workout-app/hooks/useCachedResources.ts
+++ workout-app/hooks/useCachedResources.ts
@@ -12,7 +12,10 @@ const useCachedResources = () =>
         {
             try
             {
-
+                Font.loadAsync({
+                    "montserrat": require("../assets/fonts/Montserrat-Regular.ttf"),
+                    "montserrat-bold": require("../assets/fonts/Montserrat-Bold.ttf"),
+                })
             } catch (error)
             {
                 console.warn(error);
