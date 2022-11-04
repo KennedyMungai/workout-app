68e073940cae6a5d55f945b82b51eb87beb19d7e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-04 09:27:00 +0300

    Gave the app background color of white

--- workout-app/components/WorkoutItem.tsx
+++ workout-app/components/WorkoutItem.tsx
@@ -24,6 +24,7 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         padding: 10,
         marginBottom: 10,
+        backgroundColor: "#fff",
     },
     name: {
         fontSize: 15,
