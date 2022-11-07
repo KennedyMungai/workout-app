fe573b74ec9f555afdbd802a99d4759c1bea13b7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-07 15:16:02 +0300

    Pasted the code that is in the HomeSCreen component

--- workout-app/screens/WorkoutDetailScreen.tsx
+++ workout-app/screens/WorkoutDetailScreen.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import { View, StyleSheet, FlatList, Text, Pressable } from "react-native";
+import { NativeStackHeaderProps } from '@react-navigation/native-stack';
+import data from '../data.json';
+import { WorkOut } from "../types/data";
+import WorkoutItem from "../components/WorkoutItem";
 
 
-const WorkoutDetailScreen = () =>
+export default function HomeScreen({ navigation }: NativeStackHeaderProps)
 {
     return (
-    )
-}
+        <View style={styles.container}>
+            <Text style={styles.header}>New Workouts</Text>
+            <FlatList
+                data={data as WorkOut[]}
+                keyExtractor={item => item.slug}
+                renderItem={({ item }) =>
+                {
+                    return (
+                        <Pressable
+                            onPress={() => alert(`I am pressed - ${item.name}`)}
+                        >
+                            <WorkoutItem
+                                item={item}
+                            />
+                        </Pressable>
+                    )
+                }}
+            />
+        </View>
+    );
+};
 
-export default WorkoutDetailScreen
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+        flex: 1
+    },
+    header: {
+        fontSize: 20,
+        marginBottom: 20,
+        fontWeight: "bold",
+    }
+}); 
