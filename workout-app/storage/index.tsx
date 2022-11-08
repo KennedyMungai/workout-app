5ff5f17a4cac7f09e0f77701308f88b4b95a39ec
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-08 14:27:22 +0300

    Added a try-catch block of code to tyhe storeData function

--- workout-app/storage/index.tsx
+++ workout-app/storage/index.tsx
@@ -4,7 +4,13 @@ import React from 'react';
 
 export const storeData = async () =>
 {
+    try
+    {
 
+    } catch (e)
+    {
+        console.log(e);
+    }
 };
 
 export const getData = async () =>
