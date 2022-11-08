bcb6a9b3eac95fcd8c127eea6236bb79396d154a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-08 14:31:25 +0300

    Created the stringValue constant which is the value object stringified

--- workout-app/storage/index.tsx
+++ workout-app/storage/index.tsx
@@ -6,7 +6,7 @@ export const storeData = async (key: string, value: any) =>
 {
     try
     {
-
+        const stringValue = JSON.stringify(value);
     } catch (e: any)
     {
         console.log(e);
