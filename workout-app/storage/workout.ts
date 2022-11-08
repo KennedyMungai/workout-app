035c6e682efbe7eb54e0ced867d8fdb03cc22fb5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-08 15:16:21 +0300

    Transfered the logic to yje workout file

--- workout-app/storage/workout.ts
+++ workout-app/storage/workout.ts
@@ -1,3 +1,5 @@
+import { containsKey, storeData } from ".";
+
 export const getWorkouts = () =>
 {
 
@@ -5,5 +7,11 @@ export const getWorkouts = () =>
 
 export const initWorkouts = async () =>
 {
+    const hasWorkouts = await containsKey("workout-data");
 
+    if (!hasWorkouts)
+    {
+        console.log("Storing the data");
+        await storeData("workout-data", data);
+    }
 };
