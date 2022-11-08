cfef1b8c4eba5129e0d71ee7ba521c8118293885
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-08 15:28:20 +0300

    Added a return type to the getWorkouts function

--- workout-app/storage/workout.ts
+++ workout-app/storage/workout.ts
@@ -1,8 +1,9 @@
 import { containsKey, getData, storeData } from ".";
 import data from '../data.json';
+import { WorkOut } from '../types/data';
 
 
-export const getWorkouts = async () =>
+export const getWorkouts = async (): Promise<WorkOut[]> =>
 {
     const workouts = await getData("workout-data");
     return workouts;
