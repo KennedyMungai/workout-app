1ac39cf75c2e0dfc76464b9177d47fc20bca264a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-08 15:02:27 +0300

    Added the containsKey import

--- workout-app/hooks/useCachedResources.ts
+++ workout-app/hooks/useCachedResources.ts
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import * as Font from "expo-font";
-import { getData, storeData } from '../storage';
+import { getData, storeData, containsKey } from '../storage';
 import data from "../data.json";
 
 
