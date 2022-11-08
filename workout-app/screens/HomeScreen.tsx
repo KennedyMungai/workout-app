526c26f468fc094b6b0646c6cf8a0cbc385a9028
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-08 15:55:13 +0300

    Removed the import for teh data inside the data.json file

--- workout-app/screens/HomeScreen.tsx
+++ workout-app/screens/HomeScreen.tsx
@@ -1,6 +1,5 @@
 import { View, StyleSheet, FlatList, Text, Pressable } from "react-native";
 import { NativeStackHeaderProps } from '@react-navigation/native-stack';
-import data from '../data.json';
 import { WorkOut } from "../types/data";
 import WorkoutItem from "../components/WorkoutItem";
 
