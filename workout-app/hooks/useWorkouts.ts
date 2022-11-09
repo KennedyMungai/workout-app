69a8cdb2f8ffcbd42f8a3131d6540d0a83968f42
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-09 07:53:19 +0300

    MOved the useSTate and useEffect logic to yje useWorkouts file

--- workout-app/hooks/useWorkouts.ts
+++ workout-app/hooks/useWorkouts.ts
@@ -1,9 +1,20 @@
-import React from 'react';
+import { useState, useEffect } from "react";
+import { getWorkouts } from "../storage/workout";
+import { WorkOut } from "../types/data";
 
 
 export const useWorkouts = () =>
 {
-    return (
-        
-    )
+    const [workouts, setWorkouts] = useState<WorkOut[]>([]);
+
+    useEffect(() =>
+    {
+        async function getData()
+        {
+            const _workouts = await getWorkouts();
+            setWorkouts(_workouts);
+        }
+
+        getData();
+    }, []);
 }
