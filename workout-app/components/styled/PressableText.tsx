5ce1bfa019a3592a7402ac545157382d0875c49a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-09 09:08:55 +0300

    Added a function that retruns tyhe componenr code

--- workout-app/components/styled/PressableText.tsx
+++ workout-app/components/styled/PressableText.tsx
@@ -1,4 +1,17 @@
+import React from "react"
+import { Pressable, Text } from "react-native"
+
 export const PressableText = () =>
 {
-
+    return (
+        <Pressable
+            onPress={() => alert("Opening Modal")}
+        >
+            <Text
+                style={{ textDecorationLine: "underline" }}
+            >
+                Check Sequence
+            </Text>
+        </Pressable>
+    )
 }
