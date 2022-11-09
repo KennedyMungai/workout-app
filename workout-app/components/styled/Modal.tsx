f53e2148f1349a9cf211cc7602db4da9a612a46c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-09 12:16:02 +0300

    Moved the modal code to teh Modal component

--- workout-app/components/styled/Modal.tsx
+++ workout-app/components/styled/Modal.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { View, Text } from 'react-native'
+import { PressableText } from './PressableText'
 
 
 const Modal = () =>
 {
     return (
-    
+        <Modal
+            visible={isModalVisible}
+            transparent={false}
+            animationType="slide"
+        >
+            <View style={styles.centerView}>
+                <Text>
+                    Hello There!
+                </Text>
+                <PressableText
+                    onPress={() => setIsModalVisible(false)}
+                    text="Close Modal"
+                />
+            </View>
+        </Modal >
     )
 }
 
