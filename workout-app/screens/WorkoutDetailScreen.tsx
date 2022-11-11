6c949d2b8e1fa0fef4e725e7f1795c8d32665704
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-11 11:39:55 +0300

    Added a space inside the WorkoutItem prop

--- workout-app/screens/WorkoutDetailScreen.tsx
+++ workout-app/screens/WorkoutDetailScreen.tsx
@@ -31,6 +31,7 @@ export default function WorkoutDetailScreen({ route }: Navigation)
         <View style={styles.container}>
             <WorkoutItem
                 item={workout}
+
             >
                 <Modal
                     activator={({ handleOpen }) =>
