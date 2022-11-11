42cb191a4da91506c2fcdb9af40b6a8b9d7aed30
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-11 13:57:48 +0300

    Called the clear interval for tyhe intervalRef hook

--- workout-app/hooks/useCountDown.ts
+++ workout-app/hooks/useCountDown.ts
@@ -27,7 +27,7 @@ export const useCountDown = (
 
         console.log("Tracker has been changed")
 
-        return () => window.clearInterval(intervalId)
+        return () => window.clearInterval(intervalRef.current)
 
     }, [idx])
 
