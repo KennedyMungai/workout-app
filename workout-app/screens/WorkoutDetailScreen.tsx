f8fef664b7138ff564dd22f4618f153eb3a06fce
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-11 11:24:19 +0300

    Removed tyhe hreader text

--- workout-app/screens/WorkoutDetailScreen.tsx
+++ workout-app/screens/WorkoutDetailScreen.tsx
@@ -28,7 +28,6 @@ export default function WorkoutDetailScreen({ route }: Navigation)
 
     return (
         <View style={styles.container}>
-            <Text style={styles.header}>{workout.name}</Text>
             <Modal
                 activator={({ handleOpen }) =>
                     <PressableText
