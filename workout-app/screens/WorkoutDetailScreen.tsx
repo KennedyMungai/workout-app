8a7018c4955197052e83f42abdf92df302006995
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-11 10:31:49 +0300

    Added a text component that is a child of the modal component

--- workout-app/screens/WorkoutDetailScreen.tsx
+++ workout-app/screens/WorkoutDetailScreen.tsx
@@ -35,7 +35,7 @@ export default function WorkoutDetailScreen({ route }: Navigation)
                     />
                 }
             />
-
+            <Text>Hello There</Text>
             <Modal />
         </View >
     );
