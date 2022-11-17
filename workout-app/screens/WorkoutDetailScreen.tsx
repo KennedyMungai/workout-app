fa6ca319875fb65a896ddd5432d6e1cb12f4169a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-17 12:04:12 +0300

    Replaced the code inside the whole component

--- workout-app/screens/WorkoutDetailScreen.tsx
+++ workout-app/screens/WorkoutDetailScreen.tsx
@@ -1,15 +1,15 @@
-import { View, StyleSheet, Text, Modal as DefaultModal } from "react-native";
-import { NativeStackHeaderProps } from '@react-navigation/native-stack';
+
+import { View, Text, StyleSheet } from "react-native";
+import { NativeStackHeaderProps } from "@react-navigation/native-stack";
 import { useWorkoutBySlug } from "../hooks/useWorkoutBySlug";
-import Modal from '../components/styled/Modal';
 import { PressableText } from "../components/styled/PressableText";
 import { formatSec } from "../utils/time";
 import { FontAwesome } from "@expo/vector-icons";
 import WorkoutItem from "../components/WorkoutItem";
-import { SequenceItem } from '../types/data';
 import { useEffect, useState } from "react";
-import { useCountDown } from '../hooks/useCountDown';
-
+import { SequenceItem } from "../types/data";
+import { useCountDown } from "../hooks/useCountDown";
+import Modal from '../components/styled/Modal';
 
 type DetailParams = {
     route: {
@@ -17,76 +17,54 @@ type DetailParams = {
             slug: string
         }
     }
-};
+}
 
-type Navigation = NativeStackHeaderProps & DetailParams;
+type Navigation = NativeStackHeaderProps & DetailParams
 
 export default function WorkoutDetailScreen({ route }: Navigation)
 {
-    const [sequence, setSequence] = useState<SequenceItem[]>([]);
-    const [trackerIdx, setTrackerIdx] = useState<number>(-1)
-
-    const startupSequence = ["3", "2", "1", "Go"].reverse()
-
+    const [sequence, setSequence] = useState<SequenceItem[]>([])
+    const [trackerIdx, setTrackerIdx] = useState(-1);
     const workout = useWorkoutBySlug(route.params.slug);
 
-    const { countDown, isRunning, stop, start } = useCountDown(
-        trackerIdx,
-        trackerIdx >= 0 ?
-            sequence[trackerIdx].duration :
-            -1
-    )
-
-    const hasReachedEnd: boolean =
-        sequence.length === workout!.sequence.length &&
-        countDown === 0
+    const startupSeq = ["3", "2", "1", "Go"].reverse();
+    const { countDown, isRunning, stop, start } = useCountDown(trackerIdx);
 
     useEffect(() =>
     {
-        if (!workout)
-        {
-            return
-        }
-
-        if (trackerIdx === workout.sequence.length - 1)
-        {
-            return
-        }
-
-        if (countDown === 10)
-        {
-            stop()
-        }
-
+        if (!workout) { return; }
+        if (trackerIdx === workout.sequence.length - 1) { return; }
         if (countDown === 0)
         {
             addItemToSequence(trackerIdx + 1)
         }
     }, [countDown])
 
-
     const addItemToSequence = (idx: number) =>
     {
-        let newSequence = []
+        let newSequence = [];
 
         if (idx > 0)
         {
-            newSequence = [...sequence, workout!.sequence[idx]]
-        }
-        else 
+            newSequence = [...sequence, workout!.sequence[idx]];
+        } else
         {
-            newSequence = [workout!.sequence[idx]]
+            newSequence = [workout!.sequence[idx]];
         }
 
-        setSequence(newSequence);
+        setSequence(newSequence)
         setTrackerIdx(idx)
-        start(newSequence[idx].duration + startupSequence.length)
+        start(newSequence[idx].duration + startupSeq.length);
     }
 
     if (!workout)
     {
         return null;
     }
+
+    const hasReachedEnd =
+        sequence.length === workout.sequence.length &&
+        countDown === 0
 
     return (
         <View style={styles.container}>
@@ -103,115 +81,95 @@ export default function WorkoutDetailScreen({ route }: Navigation)
                     }
                 >
                     <View>
-                        {
-                            workout.sequence.map((si, idx) => (
-                                <View
-                                    key={si.slug}
-                                    style={styles.sequenceItem}
-                                >
-                                    <Text>
-                                        {si.name} | {si.type} | {formatSec(si.duration)}
-                                    </Text>
-                                    {
-                                        idx !== workout.sequence.length - 1 &&
-                                        < FontAwesome
-                                            name="arrow-down"
-                                            size={20}
-                                        />
-                                    }
-                                </View>
-                            ))
-                        }
+                        {workout.sequence.map((si, idx) =>
+                            <View
+                                key={si.slug}
+                                style={styles.sequenceItem}
+                            >
+                                <Text>
+                                    {si.name} | {si.type} | {formatSec(si.duration)}
+                                </Text>
+                                {idx !== workout.sequence.length - 1 &&
+                                    <FontAwesome
+                                        name="arrow-down"
+                                        size={20}
+                                    />
+                                }
+                            </View>
+                        )}
                     </View>
                 </Modal>
             </WorkoutItem>
-
             <View style={styles.wrapper}>
                 <View style={styles.counterUI}>
                     <View style={styles.counterItem}>
-                        {
-                            sequence.length === 0 ?
+                        {sequence.length === 0 ?
+                            <FontAwesome
+                                name="play-circle-o"
+                                size={100}
+                                onPress={() => addItemToSequence(0)}
+                            /> :
+                            isRunning ?
+                                <FontAwesome
+                                    name="stop-circle-o"
+                                    size={100}
+                                    onPress={() => stop()}
+                                /> :
                                 <FontAwesome
                                     name="play-circle-o"
                                     size={100}
-                                    onPress={() => addItemToSequence(0)}
-                                />
-                                :
-                                isRunning
-                                    ?
-                                    <FontAwesome
-                                        name="stop-circle-o"
-                                        size={100}
-                                        onPress={() => stop()}
-                                    />
-                                    :
-                                    <FontAwesome
-                                        name="play-circle-o"
-                                        size={100}
-                                        onPress={() =>
-                                        {
-                                            if (hasReachedEnd)
-                                            {
-                                                addItemToSequence(0)
-                                            }
-                                            else 
-                                            {
-                                                start(countDown)
-                                            }
-
-                                            start(countDown)
-                                        }}
-                                    />
-                        }
-                        {
-                            sequence.length > 0 && countDown >= 0 &&
-                            <View>
-                                <Text style={styles.countDown}>
+                                    onPress={() =>
                                     {
-                                        countDown > sequence[trackerIdx].duration ?
-                                            startupSequence[countDown - sequence[trackerIdx].duration - 1] :
-                                            countDown
+                                        if (hasReachedEnd)
+                                        {
+                                            addItemToSequence(0);
+                                        } else
+                                        {
+                                            start(countDown)
+                                        }
                                     }
-                                </Text>
-                            </View>
+                                    }
+                                />
                         }
                     </View>
-                    <View style={{ alignItems: "center" }}>
-                        <Text style={{ fontSize: 60, fontWeight: "bold" }}>
-                            {
-                                sequence.length === 0 ?
-                                    "Prepare" :
-                                    hasReachedEnd ?
-                                        "Great Job" :
-                                        sequence[trackerIdx].name
-                            }
-                        </Text>
-                    </View>
+                    {sequence.length > 0 && countDown >= 0 &&
+                        <View style={styles.counterItem}>
+                            <Text style={{ fontSize: 55 }}>
+                                {
+                                    countDown > sequence[trackerIdx].duration ?
+                                        startupSeq[countDown - sequence[trackerIdx].duration - 1] :
+                                        countDown
+                                }
+                            </Text>
+                        </View>
+                    }
+                </View>
+                <View style={{ alignItems: "center" }}>
+                    <Text style={{ fontSize: 60, fontWeight: "bold" }}>
+                        {sequence.length === 0 ?
+                            "Prepare" :
+                            hasReachedEnd ?
+                                "Great Job!" : sequence[trackerIdx].name
+                        }
+                    </Text>
                 </View>
             </View>
-        </View >
-    );
-};
-
+        </View>
+    )
+}
 
 const styles = StyleSheet.create({
     container: {
         padding: 20,
-        flex: 1
+        flex: 1,
     },
     header: {
         fontSize: 20,
         marginBottom: 20,
-        fontWeight: "bold",
+        fontWeight: "bold"
     },
     sequenceItem: {
-        alignItems: "center",
-    },
-    countDown: {
-        fontSize: 50,
-        flexDirection: "row",
-        justifyContent: "space-around",
-        marginBottom: 20
+        alignItems: "center"
     },
     counterUI: {
         flexDirection: "row",
@@ -221,13 +179,13 @@ const styles = StyleSheet.create({
     },
     counterItem: {
         flex: 1,
-        alignItems: "center",
+        alignItems: "center"
     },
     wrapper: {
         borderRadius: 10,
         borderColor: "rgba(0,0,0,0.1)",
         backgroundColor: "#fff",
         borderWidth: 1,
-        padding: 10,
+        padding: 10
     }
-});
+})
