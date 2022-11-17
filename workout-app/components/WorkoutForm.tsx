5d5bf01a964a0658ae71dfadd054eb6a87e73e75
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-17 10:21:13 +0300

    Added some styling to the workout form

--- workout-app/components/WorkoutForm.tsx
+++ workout-app/components/WorkoutForm.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 
 const WorkoutForm = () =>
 {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>
                 Exercise Form
             </Text>
@@ -14,3 +14,11 @@ const WorkoutForm = () =>
 }
 
 export default WorkoutForm
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20
+    }
+})
