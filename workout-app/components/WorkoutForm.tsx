33a80743e827abf8b6b78ff0c96cbf4dd63499d8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-17 10:48:19 +0300

    Gave the textInput components some styling

--- workout-app/components/WorkoutForm.tsx
+++ workout-app/components/WorkoutForm.tsx
@@ -17,9 +17,11 @@ const WorkoutForm = () =>
             <View>
                 <TextInput
                     value={form.name}
+                    style={styles.input}
                 />
                 <TextInput
                     value={form.duration}
+                    style={styles.input}
                 />
             </View>
         </View>
@@ -34,5 +36,11 @@ const styles = StyleSheet.create({
         backgroundColor: "#fff",
         borderRadius: 10,
         padding: 10,
-    }
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
 })
