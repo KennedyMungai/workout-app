040464256710ff31649faeaa66087fa4811941df
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-17 14:58:54 +0300

    Removed unneeded spacing

--- workout-app/components/WorkoutForm.tsx
+++ workout-app/components/WorkoutForm.tsx
@@ -106,8 +106,6 @@ const WorkoutForm = ({
                                             style={styles.input}
                                         />
                                 }
-
-
                             </View>
                         }
                     />
