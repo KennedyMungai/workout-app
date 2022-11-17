9e7219c10d27d74a98b72d7ee9ed65569c237db2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-17 10:14:55 +0300

    Added some components that act as the markup of the react component

--- workout-app/components/WorkoutForm.tsx
+++ workout-app/components/WorkoutForm.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { View, Text } from 'react-native'
 
 
 const WorkoutForm = () =>
 {
     return (
-   
+        <View>
+            <Text>
+                Exercise Form
+            </Text>
+        </View>
     )
 }
 
