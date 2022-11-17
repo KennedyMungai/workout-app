5b05a88185631167cf501ff3c4fc7e74bd4ea309
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-17 10:16:09 +0300

    Deleted the useEffect statement

--- workout-app/screens/PlannerScreen.tsx
+++ workout-app/screens/PlannerScreen.tsx
@@ -1,16 +1,9 @@
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";
-import { useEffect } from "react";
 import { Text, View, Button } from "react-native";
 
 
 export default function PlannerScreen({ navigation }: NativeStackHeaderProps)
 {
-    useEffect(() =>
-    {
-        console.log("Rendering the planner screen");
-
-        return () => console.log("Unmounting the planner screen");
-    }, []);
 
 
     return (
