6e62c9632f8f46ecd52306f45af98547f3a63e3b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-17 10:18:14 +0300

    Deleted the imports for teh Text and Button react components

--- workout-app/screens/PlannerScreen.tsx
+++ workout-app/screens/PlannerScreen.tsx
@@ -1,5 +1,5 @@
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";
-import { Text, View, Button } from "react-native";
+import { View } from "react-native";
 import WorkoutForm from '../components/WorkoutForm';
 
 
