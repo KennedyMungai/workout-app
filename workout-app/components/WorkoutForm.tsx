57307d28ace19e43e72a684d7228891a17b667dd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-18 09:41:45 +0300

    Created the WorkoutForm component

---
+++ workout-app/components/WorkoutForm.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react'
+import { useForm, Controller } from 'react-hook-form'
+import { View, Text, StyleSheet, TextInput } from 'react-native'
+import { PressableText } from './styled/PressableText'
+
+
+export type WorkoutFormData = {
+    name: string,
+}
+
+type WorkoutProps = {
+    onSubmit: (form: WorkoutFormData) => void
+}
+
+const WorkoutForm = ({
+    onSubmit
+}: WorkoutProps) =>
+{
+    const { control, handleSubmit } = useForm();
+
+    return (
+        <View style={styles.container}>
+            <Controller
+                control={control}
+                rules={{
+                    required: true
+                }}
+                name="name"
+                render={({ field: { onChange, value } }) =>
+                    <TextInput
+                        onChangeText={onChange}
+                        value={value}
+                        placeholder="Name"
+                        style={styles.input}
+                    />
+                }
+            />
+        </View>
+    )
+}
+
+export default WorkoutForm
+
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#fff",
+        borderRadius: 10,
+        padding: 10,
+    },
+    input: {
+        flex: 1,
+        height: 30,
+        margin: 2,
+        borderWidth: 1,
+        padding: 5,
+        borderRadius: 10,
+        borderColor: "rgba(0,0,0,0.4)"
+    },
+    rowContainer: {
+        flexDirection: "row",
+    },
+    selection: {
+        margin: 2,
+        padding: 3,
+        alignSelf: "center"
+    }
+})
