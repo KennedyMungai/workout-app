9c8a933a35de63971e837b48b58bebb149c76fcd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-19 10:35:17 +0300

    Made the anonymous function being called onSIbmit asyncronous and made the function call for the handleWorkoutSubmit asyncronous wioth the keyword await

--- workout-app/screens/PlannerScreen.tsx
+++ workout-app/screens/PlannerScreen.tsx
@@ -108,9 +108,9 @@ export default function PlannerScreen({ navigation }: NativeStackHeaderProps)
                         ({ handleClose }) =>
                             <Text>
                                 <WorkoutForm
-                                    onSubmit={(data) =>
+                                    onSubmit={async (data) =>
                                     {
-                                        handleWorkoutSubmit(data)
+                                        await handleWorkoutSubmit(data)
                                         handleClose()
                                         navigation.navigate("Home")
                                     }}
