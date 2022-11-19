73af3e295874142bd39a09ea97dd7ad5d9fd5c54
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-19 11:01:55 +0300

    Removed the console log for the theme

--- workout-app/App.tsx
+++ workout-app/App.tsx
@@ -15,7 +15,7 @@ export default function App()
   {
     return (
       <>
-        <Navigation />
+        <Navigation colorScheme={theme} />
         <StatusBar style="auto" />
       </>
     );
