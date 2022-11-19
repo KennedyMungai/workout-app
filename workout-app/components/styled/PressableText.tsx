c50b1538cd53d1e33c4ae7ba0e3bb89be7dd2ff0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-19 11:57:49 +0300

    Made the style prop optional

--- workout-app/components/styled/PressableText.tsx
+++ workout-app/components/styled/PressableText.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { Pressable, PressableProps, StyleProp, Text, TextStyle } from "react-native"
 
-export type PressableTextProps = PressableProps & { text: string, style: StyleProp<TextStyle> }
+export type PressableTextProps = PressableProps & { text: string, style?: StyleProp<TextStyle> }
 
 export const PressableText = (props: PressableTextProps) =>
 {
