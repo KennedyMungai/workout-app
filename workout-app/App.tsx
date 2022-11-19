902067a287a341597c8956d122f3594d3a301628
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-11-19 12:11:07 +0300

    Implemented the SafeAreaProvider

--- workout-app/App.tsx
+++ workout-app/App.tsx
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import Navigation from './navigation';
 import useCachedResources from './hooks/useCachedResources';
 import { useColorScheme } from 'react-native';
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 
 export default function App()
@@ -14,10 +16,10 @@ export default function App()
   if (isLoaded)
   {
     return (
-      <>
+      <SafeAreaProvider>
         <Navigation colorScheme={colorScheme} />
         <StatusBar style="auto" />
-      </>
+      </SafeAreaProvider>
     );
   }
   else
